1d59a997a60305a5204827e3a6502564e1fe2444
korvyn <korvyn@yandex.ru>
2022-10-02 17:40:43 +0300

    Create Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число: "); 
+int первое_число = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите второе число: "); 
+int второе_число = Convert.ToInt32(Console.ReadLine()); 
+if (первое_число > второе_число) {
+    Console.Write("Максимальное число:");
+    Console.WriteLine(первое_число);
+    Console.Write("Минимальное число:");
+    Console.WriteLine(второе_число);
+}
+else {
+    Console.Write("Максимальное число:");
+    Console.WriteLine(второе_число);
+    Console.Write("Минимальное число:");
+    Console.WriteLine(первое_число);
+}
